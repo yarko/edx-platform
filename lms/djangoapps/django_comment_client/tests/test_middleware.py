b3e5ba6248e377e52ae00fafec3f6d76996b71bf
Jay Zoldak <jzoldak@edx.org>
2013-01-24 13:42:20 -0500

    More unit tests for lms.

--- lms/djangoapps/django_comment_client/tests/test_middleware.py
+++ lms/djangoapps/django_comment_client/tests/test_middleware.py
@@ -8,13 +8,10 @@ import comment_client
 import django.http
 import django_comment_client.middleware as middleware
 
-#########################################################################################
+class AjaxExceptionTestCase(TestCase):
 
-class ProcessExceptionTestCase(TestCase):
-
-#remains to check whether the correct error message is produced. the error message should
-#be the same as the argument to CommentClientError
-
+# TODO: check whether the correct error message is produced. 
+# The error message should be the same as the argument to CommentClientError
 	def setUp(self):
 		self.a = middleware.AjaxExceptionMiddleware()
 		self.request1 = django.http.HttpRequest()
@@ -23,13 +20,9 @@ class ProcessExceptionTestCase(TestCase):
 		self.exception0 = ValueError()
 		self.request1.META['HTTP_X_REQUESTED_WITH'] = "XMLHttpRequest"
 		self.request0.META['HTTP_X_REQUESTED_WITH'] = "SHADOWFAX"
-
-
 		
 	def test_process_exception(self):
 		self.assertIsInstance(self.a.process_exception(self.request1, self.exception1), middleware.JsonError)
 		self.assertIsNone(self.a.process_exception(self.request1, self.exception0))
 		self.assertIsNone(self.a.process_exception(self.request0, self.exception1))
 		self.assertIsNone(self.a.process_exception(self.request0, self.exception0))
-
-
--- lms/djangoapps/django_comment_client/tests/test_models.py
+++ lms/djangoapps/django_comment_client/tests/test_models.py
@@ -1,55 +1,54 @@
 import django_comment_client.models as models
 import django_comment_client.permissions as permissions
 from django.test import TestCase
-
+from nose.plugins.skip import SkipTest
+from courseware.courses import get_course_by_id
 
 class RoleClassTestCase(TestCase):
     def setUp(self):
-        # For course ID, syntax edx/classname/classdate is important
-        # because xmodel.course_module.id_to_location looks for a string to split
-        
         self.course_id = "edX/toy/2012_Fall"
-        self.student_role = models.Role.objects.get_or_create(name="Student", \
-                                                         course_id=self.course_id)[0]
+        self.student_role = models.Role.objects.create(name="Student",
+                                                       course_id=self.course_id)
+
+    def test_unicode(self):
+        self.assertEqual(str(self.student_role), "Student for edX/toy/2012_Fall")
+
+        self.admin_for_all = models.Role.objects.create(name="Administrator")
+        self.assertEqual(str(self.admin_for_all), "Administrator for all courses")
+
+    def test_has_permission(self):
         self.student_role.add_permission("delete_thread")
-        self.student_2_role = models.Role.objects.get_or_create(name="Student", \
-                                                         course_id=self.course_id)[0]
-        self.TA_role = models.Role.objects.get_or_create(name="Community TA",\
-                                                  course_id=self.course_id)[0]
-        self.course_id_2 = "edx/6.002x/2012_Fall"
-        self.TA_role_2 = models.Role.objects.get_or_create(name="Community TA",\
-                                                  course_id=self.course_id_2)[0]
-        class Dummy():
-            def render_template():
-                pass
-        d = {"data":{
-                "textbooks":[],
-                'wiki_slug':True,
-                }
-             }
-        
-    def testHasPermission(self):
-        # Whenever you add a permission to student_role,
-        # Roles with the same FORUM_ROLE in same class also receives the same
-        # permission.
-        # Is this desirable behavior?
+        self.TA_role = models.Role.objects.create(name="Community TA",
+                                                  course_id=self.course_id)
         self.assertTrue(self.student_role.has_permission("delete_thread"))
-        self.assertTrue(self.student_2_role.has_permission("delete_thread"))
         self.assertFalse(self.TA_role.has_permission("delete_thread"))
 
-    def testInheritPermissions(self):
-        
+    # Toy course does not have a blackout period defined.
+    def test_students_can_create_if_not_during_blackout(self):
+        self.student_role.add_permission("create_comment")
+        self.assertTrue(self.student_role.has_permission("create_comment"))
+
+    def test_students_cannot_create_during_blackout(self):
+        # Not sure how to set up these conditions
+        raise SkipTest()
+
+    def test_inherit_permissions(self):
+        self.student_role.add_permission("delete_thread")
+        self.TA_role = models.Role.objects.create(name="Community TA",
+                                                  course_id=self.course_id)
         self.TA_role.inherit_permissions(self.student_role)
         self.assertTrue(self.TA_role.has_permission("delete_thread"))
-        # Despite being from 2 different courses, TA_role_2 can still inherit
-        # permissions from TA_role without error
-        self.TA_role_2.inherit_permissions(self.TA_role)
+
+    # TODO: You should not be able to inherit permissions across courses?
+    def test_inherit_permissions_across_courses(self):
+        raise SkipTest()
+        self.student_role.add_permission("delete_thread")
+        self.course_id_2 = "MITx/6.002x/2012_Fall"
+        self.admin_role = models.Role.objects.create(name="Administrator",
+                                                     course_id=self.course_id_2)
+        self.admin_role.inherit_permissions(self.student_role)
 
 class PermissionClassTestCase(TestCase):
-
-    def setUp(self):
-        self.permission = permissions.Permission.objects.get_or_create(name="test")[0]
-
-    def testUnicode(self):
+    def test_unicode(self):
+        self.permission = permissions.Permission.objects.create(name="test")
         self.assertEqual(str(self.permission), "test")
-
