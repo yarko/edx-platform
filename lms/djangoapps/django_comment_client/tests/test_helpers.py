1758bf8a8d943a885130a8168a53362a2a562f91
Jay Zoldak <jzoldak@edx.org>
2013-01-24 13:42:20 -0500

    More unit tests for lms.

--- lms/djangoapps/django_comment_client/tests/test_helpers.py
+++ lms/djangoapps/django_comment_client/tests/test_helpers.py
@@ -4,21 +4,12 @@ import collections
 
 from django.test import TestCase                         
 
-import django_comment_client.helpers as helpers
+from django_comment_client.helpers import pluralize
 
 class PluralizeTestCase(TestCase):
 
-    def setUp(self):
-	self.term = "cat"
-
     def testPluralize(self):
+        self.term = "cat"
         self.assertEqual(pluralize(self.term, 0), "cats")
         self.assertEqual(pluralize(self.term, 1), "cat")
         self.assertEqual(pluralize(self.term, 2), "cats")
-
-#class RenderContentTestCase(TestCase):
-#	
-#	def setUp(self):
-#		self.
-       
-
