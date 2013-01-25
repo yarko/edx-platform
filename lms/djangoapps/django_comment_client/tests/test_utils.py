5dd09d313e2215f0f4d50fe91c3b67256adecb46
Jay Zoldak <jzoldak@edx.org>
2013-01-25 17:57:49 -0500

    Start to add tests for django-comment-client utils.

--- lms/djangoapps/django_comment_client/tests/test_utils.py
+++ lms/djangoapps/django_comment_client/tests/test_utils.py
@@ -2,83 +2,88 @@ import string
 import random
 import collections
 
-from django.contrib.auth.models import User
 from django.test import TestCase                         
 
-import student.models
+import factory
+from django.contrib.auth.models import User
+from student.models import UserProfile, CourseEnrollment
+from django_comment_client.models import Role, Permission
+
 import django_comment_client.models as models
 import django_comment_client.utils as utils
 
 import xmodule.modulestore.django as django
 
-#########################################################################################
+class UserFactory(factory.Factory):
+    FACTORY_FOR = User
+    username = 'robot'
+    password = '123456'
+    email = 'robot@edx.org'
+    is_active = True
+    is_staff = False
 
-class UtilsTestCase(TestCase):
-    def random_str(self, length=15, chars=string.ascii_uppercase + string.digits):
-        return ''.join(random.choice(chars) for x in range(length)) 
+class CourseEnrollmentFactory(factory.Factory):
+    FACTORY_FOR = CourseEnrollment  
+    user = factory.SubFactory(UserFactory)
+    course_id = 'edX/toy/2012_Fall'
 
-    def setUp(self):
-        self.dic1 = {}
-        self.dic2 = {}
-        self.dic2none = {}
-        self.dic2blank = {}
+class RoleFactory(factory.Factory):
+    FACTORY_FOR = Role
+    name = 'Student'
+    course_id = 'edX/toy/2012_Fall'
 
-        self.dic1["cats"] = "meow"
-        self.dic1["dogs"] = "woof"
-        self.dic1keys = ["cats", "dogs", "hamsters"]
+class PermissionFactory(factory.Factory):
+    FACTORY_FOR = Permission  
+    name = 'create_comment'
 
-        self.dic2["lions"] = "roar"
-        self.dic2["ducks"] = "quack"
-        
-        self.dic2none["lions"] = "roar"
-        self.dic2none["ducks"] = "quack"
-        self.dic2none["seaweed"] = None
-
-        self.dic2blank["lions"] = "roar"
-        self.dic2blank["ducks"] = "quack"
-        self.dic2blank["whales"] = ""   
-
-        self.course_id = "edX/toy/2012_Fall"
-
-        self.moderator_role = models.Role.objects.get_or_create(name="Moderator", course_id=self.course_id)[0]
-        self.student_role = models.Role.objects.get_or_create(name="Student", course_id=self.course_id)[0]
-
-        self.student = User.objects.create(username=self.random_str(),
-                            password="123456", email="john@yahoo.com")
-        self.moderator = User.objects.create(username=self.random_str(),
-                            password="123456", email="staff@edx.org")
-        self.moderator.is_staff = True
-        self.moderator.save()
-        self.student_enrollment = student.models.CourseEnrollment.objects.create(user=self.student, course_id=self.course_id)
-        self.moderator_enrollment = student.models.CourseEnrollment.objects.create(user=self.moderator, course_id=self.course_id)
-        self.course = "6.006"
-
+class DictionaryTestCase(TestCase):
     def test_extract(self):
-        test_extract_dic1 = {"cats": "meow", "dogs": "woof", "hamsters": None}
-        self.assertEqual(utils.extract(self.dic1, self.dic1keys), test_extract_dic1)
+        d = {'cats': 'meow', 'dogs': 'woof'}
+        k = ['cats', 'dogs', 'hamsters']
+        expected = {'cats': 'meow', 'dogs': 'woof', 'hamsters': None}
+        self.assertEqual(utils.extract(d, k), expected)
 
     def test_strip_none(self):
-        self.assertEqual(utils.strip_none(self.dic2none), self.dic2)
+        d = {'cats': 'meow', 'dogs': 'woof', 'hamsters': None}
+        expected = {'cats': 'meow', 'dogs': 'woof'}
+        self.assertEqual(utils.strip_none(d), expected)
 
     def test_strip_blank(self):
-        self.assertEqual(utils.strip_blank(self.dic2blank), self.dic2)
+        d = {'cats': 'meow', 'dogs': 'woof', 'hamsters': ' ', 'yetis': ''}
+        expected = {'cats': 'meow', 'dogs': 'woof'}
+        self.assertEqual(utils.strip_blank(d), expected)
 
-    def test_merge_dic(self):
-        self.dicMerge12 ={'cats': 'meow', 'dogs': 'woof','lions': 'roar','ducks': 'quack'}
-        self.assertEqual(utils.merge_dict(self.dic1, self.dic2), self.dicMerge12)
+    def test_merge_dict(self):
+        d1 ={'cats': 'meow', 'dogs': 'woof'}
+        d2 ={'lions': 'roar','ducks': 'quack'}
+        expected ={'cats': 'meow', 'dogs': 'woof','lions': 'roar','ducks': 'quack'}
+        self.assertEqual(utils.merge_dict(d1, d2), expected)
+
+class AccessUtilsTestCase(TestCase):
+    def setUp(self):
+        self.course_id = 'edX/toy/2012_Fall'
+        self.student_role = RoleFactory(name='Student', course_id=self.course_id)
+        self.moderator_role = RoleFactory(name='Moderator', course_id=self.course_id)
+        self.student1 = UserFactory(username='student', email='student@edx.org')
+        self.student1_enrollment = CourseEnrollmentFactory(user=self.student1)
+        self.student_role.users.add(self.student1)
+        self.student2 = UserFactory(username='student2', email='student2@edx.org')
+        self.student2_enrollment = CourseEnrollmentFactory(user=self.student2)
+        self.moderator = UserFactory(username='moderator', email='staff@edx.org', is_staff=True)
+        self.moderator_enrollment = CourseEnrollmentFactory(user=self.moderator)
+        self.moderator_role.users.add(self.moderator)
 
     def test_get_role_ids(self):
-        self.assertEqual(utils.get_role_ids(self.course_id), {u'Moderator': [2], u'Student': [1], 'Staff': [2]})
+        ret = utils.get_role_ids(self.course_id)
+        expected = {u'Moderator': [3], u'Student': [1, 2], 'Staff': [3]}
+        self.assertEqual(ret, expected)
 
-    def test_get_full_modules(self):
-        _FULLMODULES = True
-        self.assertTrue(utils.get_full_modules())
-        _FULLMODULES = False
-        self.assertEqual(utils.get_full_modules(), django.modulestore().modules)
+    def test_has_forum_access(self):
+        ret = utils.has_forum_access('student', self.course_id, 'Student')
+        self.assertTrue(ret)
 
+        ret = utils.has_forum_access('not_a_student', self.course_id, 'Student')
+        self.assertFalse(ret)
 
-    ##needs work
-    #def test_get_discussion_id_map(self):
-    #    _DISCUSSIONINFO = collections.defaultdict(list,[("6.006", False), ("18.410", True)])
-
-
+        ret = utils.has_forum_access('student', self.course_id, 'NotARole')
+        self.assertFalse(ret)
