c316fbd2ca332eba4d3cb4f882470567b7fdf982
Jay Zoldak <jzoldak@edx.org>
2013-01-25 16:33:42 -0500

    Add tests for django-comment-client permissions.

--- lms/djangoapps/django_comment_client/tests/test_permissions.py
+++ lms/djangoapps/django_comment_client/tests/test_permissions.py
@@ -2,128 +2,127 @@ import string
 import random
 import collections
 
-from django.contrib.auth.models import User
+import factory
 from django.test import TestCase                         
 
-import django_comment_client.models as models
+from django.contrib.auth.models import User
+from student.models import UserProfile, CourseEnrollment
+from django_comment_client.models import Role, Permission
+import django_comment_client.permissions as p
 
-import student.models
+class UserFactory(factory.Factory):
+    FACTORY_FOR = User
+    username = 'robot'
+    password = '123456'
+    email = 'robot@edx.org'
+    is_active = True
+    is_staff = False
 
-import django_comment_client.permissions as permissions
+class CourseEnrollmentFactory(factory.Factory):
+    FACTORY_FOR = CourseEnrollment  
+    user = factory.SubFactory(UserFactory)
+    course_id = 'edX/toy/2012_Fall'
 
-###############################################################################
+class RoleFactory(factory.Factory):
+    FACTORY_FOR = Role
+    name = 'Student'
+    course_id = 'edX/toy/2012_Fall'
+
+class PermissionFactory(factory.Factory):
+    FACTORY_FOR = Permission  
+    name = 'create_comment'
+
 
 class PermissionsTestCase(TestCase):
-    def random_str(self, length=15, chars=string.ascii_uppercase + string.digits):
-        return ''.join(random.choice(chars) for x in range(length))
-
     def setUp(self):
-
         self.course_id = "edX/toy/2012_Fall"
 
-        self.moderator_role = models.Role.objects.get_or_create(name="Moderator",
-                                                         course_id=self.course_id)[0]
-        self.student_role = models.Role.objects.get_or_create(name="Student",
-                                                       course_id=self.course_id)[0]
+        self.student_role = RoleFactory(name='Student')
+        self.moderator_role = RoleFactory(name='Moderator')
+        self.student = UserFactory(username='student', email='student@edx.org')
+        self.moderator = UserFactory(username='moderator', email='staff@edx.org', is_staff=True)
+        self.update_thread_permission = PermissionFactory(name='update_thread')
+        self.update_thread_permission.roles.add(self.student_role)
+        self.update_thread_permission.roles.add(self.moderator_role)
+        self.manage_moderator_permission = PermissionFactory(name='manage_moderator')
+        self.manage_moderator_permission.roles.add(self.moderator_role)
+        self.student_enrollment = CourseEnrollmentFactory(user=self.student)
+        self.moderator_enrollment = CourseEnrollmentFactory(user=self.moderator)
 
-        self.student = User.objects.create(username=self.random_str(),
-                            password="123456", email="john@yahoo.com")
-        self.moderator = User.objects.create(username=self.random_str(),
-                            password="123456", email="staff@edx.org")
-        self.moderator.is_staff = True
-        self.moderator.save()
-        self.student_enrollment = student.models.CourseEnrollment.objects.create(user=self.student,
-                                                                  course_id=self.course_id)
-        self.moderator_enrollment = student.models.CourseEnrollment.objects.create(user=self.moderator,
-                                                                    course_id=self.course_id)
-        #Fake json files
-        self.empty_data = {"content":{
+        self.student_open_thread = {'content': {
+                                    'closed': False,
+                                    'user_id': str(self.student.id)}
                                     }
-                    }
-        self.open_data = {"content":{
-                                "closed":False,
-                                "user_id":str(self.student.id)
-                                }
-                     }
-        self.closed_data = {"content":{
-                                "closed":True,
-                                "user_id":str(self.student.id)
-                                }
-                     }
-        
-    def tearDown(self):
-        self.student_enrollment.delete()
-        self.moderator_enrollment.delete()
+        self.student_closed_thread = {'content': {
+                                    'closed': True,
+                                    'user_id': str(self.student.id)}
+                                    }
 
-# Do we need to have this? We shouldn't be deleting students, ever
-#        self.student.delete()
-#        self.moderator.delete()
+    def test_user_has_permission(self):
+        s_ut = p.has_permission(self.student, 'update_thread', self.course_id)
+        m_ut = p.has_permission(self.moderator, 'update_thread', self.course_id)
+        s_mm = p.has_permission(self.student, 'manage_moderator', self.course_id)
+        m_mm = p.has_permission(self.moderator, 'manage_moderator', self.course_id)
+        self.assertTrue(s_ut)
+        self.assertTrue(m_ut)
+        self.assertFalse(s_mm)
+        self.assertTrue(m_mm)
 
+    def test_check_conditions(self):
+        # Checks whether the discussion thread is open, or whether the author is user
+        s_o = p.check_condition(self.student, 'is_open', self.course_id, self.student_open_thread)
+        s_a = p.check_condition(self.student, 'is_author', self.course_id, self.student_open_thread)
+        m_c = p.check_condition(self.moderator, 'is_open', self.course_id, self.student_closed_thread)
+        m_a = p.check_condition(self.moderator,'is_author', self.course_id, self.student_open_thread)
+        self.assertTrue(s_o)
+        self.assertTrue(s_a)
+        self.assertFalse(m_c)
+        self.assertFalse(m_a)
 
-    def testDefaultRoles(self):
-        self.assertTrue(self.student_role in self.student.roles.all())
-        self.assertTrue(self.moderator_role in self.moderator.roles.all())
+    def test_check_conditions_and_permissions(self):
+        # Check conditions
+        ret = p.check_conditions_permissions(self.student,
+                                            'is_open',
+                                            self.course_id,
+                                            data=self.student_open_thread)
+        self.assertTrue(ret)
 
-    def testPermission(self):
-        name = self.random_str()
-        self.moderator_role.add_permission(name)
-        self.assertTrue(permissions.has_permission(self.moderator, name, self.course_id))
-        # Moderators do not have student priveleges unless explicitly added
+        # Check permissions
+        ret = p.check_conditions_permissions(self.student, 
+                                            'update_thread',
+                                            self.course_id,
+                                            data=self.student_open_thread)
+        self.assertTrue(ret)
 
-        self.student_role.add_permission(name)
-        self.assertTrue(permissions.has_permission(self.student, name, self.course_id))
+        # Check that a list of permissions/conditions will be OR'd
+        ret = p.check_conditions_permissions(self.moderator, 
+                                            ['is_open','manage_moderator'],
+                                            self.course_id,
+                                            data=self.student_open_thread)
+        self.assertTrue(ret)
 
-        # Students don't have moderator priveleges 
-        name2 = self.random_str()
-        self.student_role.add_permission(name2)
-        self.assertFalse(permissions.has_permission(self.moderator, name2, self.course_id))
+        # Check that a list of permissions will be OR'd
+        ret = p.check_conditions_permissions(self.student, 
+                                            ['update_thread','manage_moderator'],
+                                            self.course_id,
+                                            data=self.student_open_thread)
+        self.assertTrue(ret)
 
-    def testCachedPermission(self):
-        # Cache miss returns None
-        # Don't really understand how this works? What's in Cache?
-        self.assertFalse(permissions.cached_has_permission(self.student, self.moderator,
-                                            course_id=None))
-        self.assertFalse(permissions.cached_has_permission(self.student, "update_thread",
-                                            course_id=None))
+        # Check that a list of list of permissions will be AND'd
+        ret = p.check_conditions_permissions(self.student, 
+                                            [['update_thread','manage_moderator']],
+                                            self.course_id,
+                                            data=self.student_open_thread)
+        self.assertFalse(ret)
 
-    def testCheckCondition(self):
-        # Checks whether something? is open, or whether the author is user
-        self.assertFalse(permissions.check_condition(self.student, 'is_open',
-                                         self.course_id, self.empty_data))
-        self.assertFalse(permissions.check_condition(self.student, 'is_author',
-                                         self.course_id, self.empty_data))
-        self.assertTrue(permissions.check_condition(self.student,'is_open',
-                                         self.course_id, self.open_data))
-        self.assertTrue(permissions.check_condition(self.student, 'is_author',
-                                         self.course_id, self.open_data))
-        self.assertFalse(permissions.check_condition(self.student,'is_open', 
-                                         self.course_id, self.closed_data))
+    def test_check_permissions_by_view(self):
+        ret = p.check_permissions_by_view(self.student, self.course_id,
+                                    self.student_open_thread, 'openclose_thread')
+        self.assertFalse(ret)
 
-    def testCheckConditionsPermissions(self):
-        #Function does not seem to return True
-        self.assertFalse(permissions.check_conditions_permissions(self.student, 'is_open', 
-                                                     self.course_id,
-                                                     data=self.open_data))
-        self.assertFalse(permissions.check_conditions_permissions(self.student, 'is_open', 
-                                                     self.course_id,
-                                                     data=self.empty_data))
-
-        self.assertFalse(permissions.check_conditions_permissions(self.student, 
-                                                      ['is_open', 'is_author'],
-                                                      self.course_id,
-                                                      data=self.open_data))
-        self.assertFalse(permissions.check_conditions_permissions(self.student, 
-                                                      ['is_open', 'is_author'],
-                                                      self.course_id,
-                                                      data=self.open_data,
-                                                      operator='and'))
-        self.assertFalse(permissions.check_conditions_permissions(self.student,'update_thread',
-                                                      self.course_id, data=self.open_data))
-
-    def testCheckPermissionsByView(self):
-        # kwargs is the data entered in check_condition, which is json?
-        self.assertRaises(UnboundLocalError, permissions.check_permissions_by_view,
-                          self.student, self.course_id,self.empty_data,
-                          "nonexistant")
-        self.assertFalse(permissions.check_permissions_by_view(self.student,self.course_id, 
-                                                   self.empty_data, 'update_thread'))
+        # Check a view permission that includes both a condition and a permission
+        self.vote_permission = PermissionFactory(name='vote')
+        self.vote_permission.roles.add(self.student_role)
+        ret = p.check_permissions_by_view(self.student, self.course_id,
+                                    self.student_open_thread, 'vote_for_comment')
+        self.assertTrue(ret)
