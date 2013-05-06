11973347e9872b9c0592d700c3e2dceaac16652a
Will Daly <will@will-VirtualBox.(none)>
2013-05-06 14:09:37 -0400

    Changed fuzz test to call safe_exec directly

--- lms/djangoapps/courseware/tests/fuzz_tests/custom_response/fuzz_custom_response.py
+++ lms/djangoapps/courseware/tests/fuzz_tests/custom_response/fuzz_custom_response.py
@@ -1,10 +1,7 @@
 """ Fuzz test for CustomResponse """
 
-import capa.capa_problem as lcp
-from capa.responsetypes import StudentInputError, ResponseError, LoncapaProblemError
-from xmodule.x_module import ModuleSystem
-import mock
-import fs.osfs
+from capa.safe_exec import safe_exec
+from codejail.safe_exec import SafeExecException
 import random
 
 from datetime import datetime
@@ -12,7 +9,6 @@ import sys
 import traceback
 import string
 import os
-import lxml
 import struct
 
 # Use memcache running locally
@@ -30,44 +26,64 @@ settings.configure(CACHES=CACHE_SETTINGS)
 
 from django.core.cache import cache
 
+# Script to install as the checker for the CustomResponse
+TEST_SCRIPT = u"""
+# Import a library
+import numpy
+
+def check_func(expect, answer_given):
+    # Looping and printin
+    for i in range(0, 1234):
+        print "Test"
+        x = 5
+
+    # While loop
+    i = 0
+    while i < 10:
+        i += 1
+
+    # Return a dict
+    return {'ok': answer_given == expect, 'msg': 'Message text'}
+"""
+
 PRINTABLE_CHARS = [c for c in string.printable]
-#UNICODE_CHARS = [unichr(u) for u in range(0, 2**16)]
-ALL_CHARS = PRINTABLE_CHARS #+ UNICODE_CHARS
+UNICODE_CHARS = [unichr(u) for u in range(0, 2**16)]
+ALL_CHARS = PRINTABLE_CHARS + UNICODE_CHARS
 def random_string(length=random.randint(0, 10)):
     """ Return a random string of the given length """
     chars = [random.choice(ALL_CHARS) for i in range(length)]
     return "".join(chars)
 
 MAX_NUM_MUTATIONS = 5
-def mutate_xml(xml):
-    """ Mutate the `xml` string randomly, then return the mutated string """
+def mutate_string(orig_string):
+    """ Mutate `string` randomly, then return the mutated string """
 
     # Copy the string
-    mutated_xml = xml
+    mutated_string = orig_string
 
     # Mutate a random number of characters
     for i in range(0, random.randint(1, MAX_NUM_MUTATIONS)):
         
         # Choose a random index in the string
-        index = random.randint(1, len(xml) - 1)
+        index = random.randint(1, len(mutated_string) - 1)
 
         # Choose a random character to change it to
         new_char = random.choice(ALL_CHARS)
 
         # Replace the character 
-        mutated_xml = mutated_xml[0:index-1] + new_char + mutated_xml[index+1:]
+        mutated_string = mutated_string[0:index-1] + new_char + mutated_string[index+1:]
 
-    return mutated_xml
+    return mutated_string
 
-def generate_random_inputs(seed, problems):
+def generate_random_inputs(seed):
     """ Deterministically generate input values for the fuzz test 
     from the random `seed`.
     
     Returns a dict with keys:
         
-    * `problem_xml` (string)
-    * `problem_seed` (int)
-    * `student_submission` (string)
+    * `problem_code` (string)
+    * `random_seed` (int)
+    * `globals_dict` (dictionary of globals)
     """
 
     # Seed the PRNG to get repeatable outputs
@@ -76,97 +92,54 @@ def generate_random_inputs(seed, problems):
     # Create a dict to store the inputs
     inputs = {}
 
-    # Create problem XML by mutating an existing problem
-    inputs['problem_xml'] = mutate_xml(random.choice(problems))
+    # Create problem code by mutating existing problem
+    inputs['problem_code'] = mutate_string(TEST_SCRIPT)
 
-    # Random problem seed
-    inputs['problem_seed'] = random.randint(0, 100000)
+    # Random seed for the execution
+    inputs['random_seed'] = random.randint(-10000, 10000)
 
-    # Random student submission string
-    inputs['student_submission'] = random_string()
+    # Random globals dict
+    inputs['globals_dict'] = {}
+    for i in range(random.randint(1, 100)):
+        inputs['globals_dict'][random_string()] = random_string()
 
     return inputs
 
-def system_mock():
-    """ Create a mock ModuleSystem, installing our cache"""
-    system = mock.MagicMock(ModuleSystem)
-    system.render_template = lambda template, context: "<div>%s</div>" % template
-    system.cache = cache
-    system.filestore = mock.MagicMock(fs.osfs.OSFS)
-    system.filestore.root_path = ""
-    system.DEBUG = True
-
-    return system
-
-def answer_ids(problem):
-    """ Given a loncapa problem `problem`, return a list of ID's to be used when
-    submitting answers """
-    nested_ids = problem.get_answer_ids()
-    all_ids = ['1_2_1', '1_2_2', '1_3_1', '1_3_2']
-
-    for sublist in nested_ids:
-        all_ids.extend(sublist)
-
-    return all_ids
-
-NO_ERROR, INVALID_XML, PROBLEM_SPECIFIC_ERROR, UNEXPECTED_ERROR = range(4)
-def execute_test(problem_xml="", problem_seed=1, student_submission=""):
+NO_ERROR, SAFE_EXEC_ERROR, UNEXPECTED_ERROR = range(3)
+def execute_test(problem_code="", random_seed=1, globals_dict=None):
     """ Execute the test using input arguments.
     Returns `(result, description)` tuple, where
-    `result` is either INVALID_XML, PROBLEM_SPECIFIC_ERROR, UNEXPECTED_ERROR, or NO_ERROR
+    `result` is either NO_ERROR, SAFE_EXEC_ERROR, or UNEXPECTED_ERROR
     and `description` is a string describing the error """
 
-    # Create the problem
     try:
-        problem = lcp.LoncapaProblem(problem_xml, '1', state=None,
-                                     seed=problem_seed, system=system_mock())
+        safe_exec(problem_code, globals_dict, random_seed=random_seed, cache=cache)
 
-    # If we've generated invalid XML, then don't bother testing it
-    except lxml.etree.XMLSyntaxError:
-        return (INVALID_XML, None)
+    # Ignore safe exec exceptions
+    except SafeExecException:
+        return (SAFE_EXEC_ERROR, None)
 
-    # If we get a problem-specific error, report it
-    except (StudentInputError, ResponseError, LoncapaProblemError):
-        return (PROBLEM_SPECIFIC_ERROR, None)
-
+    # Exit if we're interrupted
     except KeyboardInterrupt:
         raise
 
-    # Otherwise, we've broken the problem
+    # Look out for unexpected errors
     except:
         return (UNEXPECTED_ERROR, traceback.format_exc())
 
-    # Submit an answer and try to grade the problem
-    keys = answer_ids(problem)
-    values = [student_submission for i in range(len(keys))]
-    answers = dict(zip(keys, values))
-
-    try:
-        problem.grade_answers(answers)
-
-    # Ignore expected, capa-specific errors
-    except (StudentInputError, ResponseError, LoncapaProblemError):
-        return (PROBLEM_SPECIFIC_ERROR, None)
-
-    except KeyboardInterrupt:
-        raise
-
-    except:
-        # Report all other exceptions
-        return (UNEXPECTED_ERROR, traceback.format_exc())
-
+    # Otherwise, no error
     else:
         return (NO_ERROR, None)
 
-def run_fuzz_tests(problems):
+def run_fuzz_tests():
     """ Randomize student submission, check function, and hint function
     to see if we can break CustomResponse """
 
     # Maintain counts so we can figure out how far our inputs make it
     iteration_count = 0
-    invalid_xml_count = 0
-    problem_error_count = 0
+    safe_exec_error_count = 0
     unexpected_error_count = 0
+    no_error_count = 0
 
     # Create a (hopefully unique) file to write outputs to
     output_filename = 'results_{0}.txt'.format(str(datetime.now()))
@@ -178,7 +151,7 @@ def run_fuzz_tests(problems):
             seed = struct.unpack('i', os.urandom(4))[0]
 
             # Generate random inputs
-            inputs = generate_random_inputs(seed, problems)
+            inputs = generate_random_inputs(seed)
 
             # Execute the test and see if we get an unexpected exception
             (result, description) = execute_test(**inputs)
@@ -188,11 +161,11 @@ def run_fuzz_tests(problems):
                 output_file.write("\nERROR {0}: {1}\n".format(seed, description))
                 unexpected_error_count += 1
 
-            elif result == INVALID_XML:
-                invalid_xml_count += 1
+            elif result == SAFE_EXEC_ERROR:
+                safe_exec_error_count += 1
 
-            elif result == PROBLEM_SPECIFIC_ERROR:
-                problem_error_count += 1
+            else:
+                no_error_count += 1
 
             # Print a status update every 100 iterations
             iteration_count += 1
@@ -201,63 +174,21 @@ def run_fuzz_tests(problems):
                 output_file.flush()
 
         except KeyboardInterrupt:
-            msg = "Stopping tests at iteration {0} ({1} xml errors, {2} problem-specific errors, {3} unexpected errors) ..."
-            print msg.format(iteration_count, invalid_xml_count, problem_error_count, unexpected_error_count)
+            msg = "Stopping tests at iteration {0} ({1} safe exec errors, {2} unexpected errors, {3} successes) ..."
+            print msg.format(iteration_count, safe_exec_error_count, unexpected_error_count, no_error_count)
             output_file.close()
             exit(0)
-
-PROBLEMS_DIR = "problems"
-def load_existing_problems():
-    """ Recursively walk the problems/ directory, loading XML files as strings.
-    Returns a list of XML strings """
-
-    problems = []
-    paths = []
-
-    def visit_func(arg, dirname, names):
-        for filename in names:
-            fullpath = os.path.join(dirname, filename)
-            if os.path.isfile(fullpath):
-                (_, ext) = os.path.splitext(filename)
-                if ext == ".xml":
-                    paths.append(fullpath)
-                else:
-                    pass
-            else:
-                pass
-
-    os.path.walk(PROBLEMS_DIR, visit_func, None)
-
-    for path in sorted(paths):
-        with open(path) as problem_file:
-            try:
-                problem_str = problem_file.read()
-            except:
-                pass
-            else:
-                problems.append(problem_str)
-
-    return problems
-
-PROBLEM_TAG = 'customresponse'
-def filter_problems(problems):
-    """ Filter out non custom-response problems """
-    return [xml for xml in problems if PROBLEM_TAG in xml]
 
 def main():
     """ If we have arguments, interpret them as random seeds and print out the inputs.
     Otherwise, run fuzz tests until the user interrupts """
 
-    # Pre-load existing problems, which we will mutate later
-    # Filter out non-custom response problems
-    problems = filter_problems(load_existing_problems())
-
     if len(sys.argv) > 1:
         for seed in [int(arg) for arg in sys.argv[1:]]:
-            inputs = generate_random_inputs(seed, problems)
+            inputs = generate_random_inputs(seed)
             print "{0}: {1}".format(seed, str(inputs))
     else:
-        run_fuzz_tests(problems)
+        run_fuzz_tests()
 
 if __name__ == '__main__':
     main()
