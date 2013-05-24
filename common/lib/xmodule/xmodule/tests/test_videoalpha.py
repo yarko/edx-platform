9974d2c423846a7d5e1b66a0eef7cfcfa0fc7e83
Alexander Kryklia <kryklia@gmail.com>
2013-05-24 14:20:31 +0300

    adds docstrings for video alpha tests

--- common/lib/xmodule/xmodule/tests/test_videoalpha.py
+++ common/lib/xmodule/xmodule/tests/test_videoalpha.py
@@ -13,11 +13,13 @@ class VideoAlphaModuleTest(LogicTest):
     }
 
     def test_get_timeframe_no_parameters(self):
+        "Make sure that timeframe() works correctly w/o parameters"
         xmltree = etree.fromstring('<videoalpha>test</videoalpha>')
         output = self.xmodule._get_timeframe(xmltree)
         self.assertEqual(output, ('', ''))
 
     def test_get_timeframe_with_one_parameter(self):
+        "Make sure that timeframe() works correctly with one parameter"
         xmltree = etree.fromstring(
             '<videoalpha start_time="00:04:07">test</videoalpha>'
         )
@@ -25,6 +27,7 @@ class VideoAlphaModuleTest(LogicTest):
         self.assertEqual(output, (247, ''))
 
     def test_get_timeframe_with_two_parameters(self):
+        "Make sure that timeframe() works correctly with two parameters"
         xmltree = etree.fromstring(
             '''<videoalpha
                     start_time="00:04:07"
