3dd41065981b39ee49426e0e2032ee76353c9db4
Alexander Kryklia <kryklia@gmail.com>
2013-05-27 11:37:01 +0300

    adds test files for video

--- common/lib/xmodule/xmodule/tests/test_video_xml.py
+++ common/lib/xmodule/xmodule/tests/test_video_xml.py
@@ -1,134 +1,146 @@
 # -*- coding: utf-8 -*-
-"""Test for Video Xmodule functional logic.
-These tests data readed from xml, not from mongo.
+"""Test for Video Alpha Xmodule functional logic.
+These tests data readed from  xml, not from mongo.
 
-We have a ModuleStoreTestCase class defined in
-common/lib/xmodule/xmodule/modulestore/tests/django_utils.py.
-You can search for usages of this in the cms and lms tests for examples.
-You use this so that it will do things like point the modulestore
-setting to mongo, flush the contentstore before and after, load the
-templates, etc.
-You can then use the CourseFactory and XModuleItemFactory as defined in
-common/lib/xmodule/xmodule/modulestore/tests/factories.py to create the
-course, section, subsection, unit, etc.
+we have a ModuleStoreTestCase class defined in common/lib/xmodule/xmodule/modulestore/tests/django_utils.py. You can search for usages of this in the cms and lms tests for examples. You use this so that it will do things like point the modulestore setting to mongo, flush the contentstore before and after, load the templates, etc.
+You can then use the CourseFactory and XModuleItemFactory as defined in common/lib/xmodule/xmodule/modulestore/tests/factories.py to create the course, section, subsection, unit, etc.
+
+
+
+
+
 """
 
-import json
-import unittest
-from mock import Mock
-from lxml import etree
-
-from xmodule.video_module import VideoDescriptor, VideoModule
+from xmodule.videoalpha_module import VideoAlphaDescriptor, VideoAlphaModule
+from . import PostData, LogicTest, etree
 from xmodule.modulestore import Location
-
+from mock import Mock
 from . import test_system
-from .test_logic import LogicTest
 
+class VideoAlphaFactory(object):
 
-class VideoFactory(object):
-    """A helper class to create video modules with various parameters
-    for testing.
+    """
+    A helper class to create videoalpha modules with various parameters for testing.
     """
 
     # tag that uses youtube videos
     sample_problem_xml_youtube = """
-        <video show_captions="true"
+        <videoalpha show_captions="true"
         youtube="0.75:jNCf2gIqpeE,1.0:ZwkTiUPN0mg,1.25:rsq9auxASqI,1.50:kMyNdzVHHgg"
         data_dir=""
+        show_captions="true"
         caption_asset_path=""
         autoplay="true"
         from="01:00:03" to="01:00:10"
         >
             <source src=".../mit-3091x/M-3091X-FA12-L21-3_100.mp4"/>
-        </video>
+        </videoalpha>
+    """
+    # tag that uses html5 video
+    sample_problem_xml_html5 = """
+        <videoalpha show_captions="true"
+        youtube=""
+        sub="jNCf2gIqpeE12345"
+        from="01:00:03" to="01:00:10"
+        show_captions="true"
+        caption_asset_path=""
+        autoplay="true"
+        >
+            <source src=".../mit-3091x/M-3091X-FA12-L21-3_100.mp4"/>
+            <source src=".../mit-3091x/M-3091X-FA12-L21-3_100.webm"/>
+            <source src=".../mit-3091x/M-3091X-FA12-L21-3_100.ogv"/>
+
+        </videoalpha>
+    """
+
+    sample_problem_xml = """<videoalpha> </videoalpha>
     """
 
     @staticmethod
-    def create():
-        """Method return Video Xmodule instance."""
-        location = Location(["i4x", "edX", "video", "default",
+    def create(youtube=None,
+               sub=None,
+               autoplay=None,
+               show_captions=None,
+               data_dir=None,
+               caption_asset_path=None,
+               time_from=None,
+               time_to=None,
+               ):
+        """
+        All parameters are optional, and are added to the created problem if specified.
+
+        Arguments:
+            TODO: describe
+        """
+        location = Location(["i4x", "edX", "videoalpha_test", "videoalpha",
                              "SampleProblem{0}".format(1)])
-        model_data = {'data': VideoFactory.sample_problem_xml_youtube}
+        model_data = {'data': VideoAlphaFactory.sample_problem_xml}
+
+        if youtube is not None:
+            model_data['youtube'] = youtube
+        if sub is not None:
+            model_data['sub'] = sub
+        if autoplay is not None:
+            model_data['autoplay'] = autoplay
+        if show_captions is not None:
+            model_data['show_captions'] = show_captions
+        if data_dir is not None:
+            model_data['data_dir'] = data_dir
+        if caption_asset_path is not None:
+            model_data['caption_asset_path'] = caption_asset_path
+        if time_from is not None:
+            model_data['from'] = time_from
+        if time_to is not None:
+            model_data['to'] = time_to
 
         descriptor = Mock(weight="1")
 
         system = test_system()
-        system.render_template = lambda template, context: context
-        module = VideoModule(system, location, descriptor, model_data)
+        system.render_template = Mock(return_value="<div>Test Template HTML</div>")
+        module = VideoAlphaModule(system, location, descriptor, model_data)
 
         return module
 
-
-class VideoModuleUnitTest(LogicTest):
-    descriptor_class = VideoDescriptor
+class VideoAlphaModuleTest(LogicTest):
+    descriptor_class = VideoAlphaDescriptor
 
     raw_model_data = {
-        'data': '<video />'
+        'data': '<videoalpha />'
     }
 
+
+
+
     def test_get_timeframe_no_parameters(self):
-        """Make sure that timeframe() works correctly w/o parameters"""
-        xmltree = etree.fromstring('<video>test</video>')
+        "Make sure that timeframe() works correctly w/o parameters"
+        xmltree = etree.fromstring('<videoalpha>test</videoalpha>')
         output = self.xmodule._get_timeframe(xmltree)
         self.assertEqual(output, ('', ''))
 
     def test_get_timeframe_with_one_parameter(self):
-        """Make sure that timeframe() works correctly with one parameter"""
+        "Make sure that timeframe() works correctly with one parameter"
         xmltree = etree.fromstring(
-            '<video from="00:04:07">test</video>'
+            '<videoalpha start_time="00:04:07">test</videoalpha>'
         )
         output = self.xmodule._get_timeframe(xmltree)
         self.assertEqual(output, (247, ''))
 
     def test_get_timeframe_with_two_parameters(self):
-        """Make sure that timeframe() works correctly with two parameters"""
+        "Make sure that timeframe() works correctly with two parameters"
         xmltree = etree.fromstring(
-            '''<video
-                    from="00:04:07"
-                    to="13:04:39"
-                >test</video>'''
+            '''<videoalpha
+                    start_time="00:04:07"
+                    end_time="13:04:39"
+                >test</videoalpha>'''
         )
         output = self.xmodule._get_timeframe(xmltree)
         self.assertEqual(output, (247, 47079))
 
-
-class VideoModuleItegrationTest(unittest.TestCase):
     def test_video_constructor(self):
-        """Make sure that all parameters extracted correclty from xml"""
-        module = VideoFactory.create()
+        "Make sure that all parameters extracted correclty from xml"
+        # output = self.xmodule
+        module = VideoAlphaFactory.create()
+        import ipdb; ipdb.set_trace()
 
-        # `get_html` return only context, cause we
-        # overwrite `system.render_template`
-        context = module.get_html()
-        expected_context = {
-            'track': None,
-            'show_captions': 'true',
-            'display_name': 'SampleProblem1',
-            'id': 'i4x-edX-video-default-SampleProblem1',
-            'end': 3610.0,
-            'caption_asset_path': '/static/subs/',
-            'source': '.../mit-3091x/M-3091X-FA12-L21-3_100.mp4',
-            'streams': '0.75:jNCf2gIqpeE,1.0:ZwkTiUPN0mg,1.25:rsq9auxASqI,1.50:kMyNdzVHHgg',
-            'normal_speed_video_id': 'ZwkTiUPN0mg',
-            'position': 0,
-            'start': 3603.0
-        }
-        self.assertDictEqual(context, expected_context)
 
-        self.assertEqual(
-            module.youtube,
-            '0.75:jNCf2gIqpeE,1.0:ZwkTiUPN0mg,1.25:rsq9auxASqI,1.50:kMyNdzVHHgg')
-
-        self.assertEqual(
-            module.video_list(),
-            module.youtube)
-
-        self.assertEqual(
-            module.position,
-            0)
-
-        self.assertDictEqual(
-            json.loads(module.get_instance_state()),
-            {'position': 0})
-
-        self.assertIsNone(module.get_progress())
+#
