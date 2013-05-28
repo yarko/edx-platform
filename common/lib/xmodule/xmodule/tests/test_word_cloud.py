6852baab95c45506395ae128f186cff08236b95d
Alexander Kryklia <kryklia@gmail.com>
2013-05-28 12:17:30 +0300

    adds finished test for word cloud

--- common/lib/xmodule/xmodule/tests/test_word_cloud.py
+++ common/lib/xmodule/xmodule/tests/test_word_cloud.py
@@ -80,24 +80,51 @@ class BaseTestXmodule(ModuleStoreTestCase):
 class TestWordCloud(BaseTestXmodule):
     TEMPLATE_NAME = "i4x://edx/templates/word_cloud/Word_cloud"
 
-    def test_per_user(self):
-        """Test word cloud data flow per single user actions.
+    def test_single_and_collective_users_submits(self):
+        """Test word cloud data flow per single and collective users submits.
 
             Make sures that:
 
             1. Inital state of word cloud is correct. Those state that
-            is sended from server to frontend, when student loads word
+            is sended from server to frontend, when students load word
             cloud page.
 
-            2. Student can submit data succesfully.
+            2. Students can submit data succesfully.
 
-            3. Word cloud data properly updates after student's submit.
+            3. Word cloud data properly updates after students submit.
 
-            4. Next submits produce "already voted" error
+            4. Next submits produce "already voted" error. Next submits for user
+            are not allowed by user interface, but techically it possible, and
+            word_cloud should properly react.
 
-            5. State of word cloud is still as in #3.
+            5. State of word cloud after #4 is still as in #3.
 
         """
+
+        def check_normal_word_cloud_response(response_contents, correct_jsons):
+            """Utility function that compares correct and real responses."""
+            for user, content in response_contents.items():
+
+                # used in debugger for comparing dictionaries
+                # self.maxDiff = None
+
+                # we should compare top_words for manually, because they are unsorted
+                keys_to_compare = set(content.keys()).difference(set(['top_words']))
+                self.assertDictEqual(
+                    {k: content[k] for k in keys_to_compare},
+                    {k: correct_jsons[user][k] for k in keys_to_compare})
+
+                # comparing top_words:
+                top_words_content = sorted(
+                    content['top_words'],
+                    key=itemgetter('text')
+                )
+                top_words_correct = sorted(
+                    correct_jsons[user]['top_words'],
+                    key=itemgetter('text')
+                )
+                self.assertListEqual(top_words_content, top_words_correct)
+
         # check word cloud response for every user
         responses = {
             user: self.clients[user].post(self.get_url('get_state'))
@@ -110,16 +137,11 @@ class TestWordCloud(BaseTestXmodule):
             responses.items()
         }
         self.assertEqual(
-            ''.join(
-                set(
-                    [
+            ''.join(set([
                         content['status']
                         for _, content in response_contents.items()
-                    ]
-                )
-            ),
-            'success'
-        )
+                        ])),
+            'success')
 
         # 1)
         # correct initial data:
@@ -177,22 +199,45 @@ class TestWordCloud(BaseTestXmodule):
                 ]
             }
 
-        for user, content in response_contents.items():
-            self.maxDiff = None
+        # 3)
+        check_normal_word_cloud_response(response_contents, correct_jsons)
 
-            # we should compare top_words for manually, because they are unsorted
-            keys_to_compare = set(content.keys()).difference(set(['top_words']))
-            self.assertDictEqual(
-                {k: content[k] for k in keys_to_compare},
-                {k: correct_jsons[user][k] for k in keys_to_compare})
+        # 4)
+        response_contents = {}
+        for index, user in enumerate(self.users):
+            response = self.clients[user].post(
+                self.get_url('submit'),
+                {'student_words[]': input_words},
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+            response_contents[user] = json.loads(response.content)
 
-            # comparing top_words:
-            top_words_content = sorted(
-                content['top_words'],
-                key=itemgetter('text')
-            )
-            top_words_correct = sorted(
-                correct_jsons[user]['top_words'],
-                key=itemgetter('text')
-            )
-            self.assertListEqual(top_words_content, top_words_correct)
+        self.assertEqual(
+            ''.join(set([
+                        content['status']
+                        for _, content in response_contents.items()
+                        ])),
+            'fail')
+
+        # 5)
+        response_contents = {}
+        correct_jsons = {}
+        for index, user in enumerate(self.users):
+            response = self.clients[user].post(self.get_url('get_state'))
+            response_contents[user] = json.loads(response.content)
+
+            correct_jsons[user] = {
+                u'status': u'success',
+                u'submitted': True,
+                u'display_student_percents': True,
+                u'student_words': {word: self.USER_COUNT for word in correct_words},
+                u'total_count': len(input_words) * self.USER_COUNT,
+                u'top_words': [
+                    {
+                        u'text': word, u'percent': 100 / len(input_words),
+                        u'size': self.USER_COUNT
+                    }
+                    for word in correct_words
+                ]
+            }
+        check_normal_word_cloud_response(response_contents, correct_jsons)
