69438fd226e975a8086eb526611a22561b7a277e
Julian Arni <julian@edx.org>
2013-05-29 16:42:11 -0400

    Added jsinput constructor.

--- common/static/js/capa/jsinput.js
+++ common/static/js/capa/jsinput.js
@@ -1,26 +1,57 @@
 (function () {
+    var timeout = 120;
 
-    initialize
-    function initialize(elem) {
-    // Find DOM elements that are needed for initialization, then call the
-    // script, passing the relevant data.
-        var parent = $(elem).parent();
-        var input_field = parent.find('input[id*="jsinput"]');
-        var value = input_field.val();
-        var placeholder = parent.find(".script_placeholder");
-        var checkfn = $(placeholder).data('check');
-        var src = $(placeholder).data('src');
-        loadScript(src);
+    // This function should only run once, even if there are multiple jsinputs
+    // on a page.
+    if (typeof(_editamolecule_loaded) == 'undefined' || _editamolecule_loaded === false) {
+        _editamolecule_loaded = true;
+        loadGWTScripts();
+        waitForGWT();
+    } else {
+        return;
     }
 
-    function loadScript(src) {
-        var script = document.createElement('script');
-        script.setAttribute('type', 'text/javascript');
-        script.setAttribute('src',  src);
-        $('head')[0].appendChild(script);
-    }
+    var allElems = [] ;
 
-    function preSubmit(checkfn, input_field) {
-        input_field.val(checkfn())
-    }
-})
+    // Define an class (functional pattern) that will be instantiated for each
+    // jsinput element of the DOM
+    var jsinputConstructor = function (spec, clsm) {
+        // 'that' is the object returned by the constructor. It holds public
+        // methods
+        var that;
+
+
+        // private methods
+        var getParent = function (elem) { return $(elem).parent() ; };
+        var inputfield =  function (parent, id) {
+            return parent.find('input[id=jsinput' + id);
+        };
+
+        // public methods
+        that.id = function () { return spec.id ; };
+        that.elem = function () { return spec.elem ; };
+        that.parent = function () { return getParent(spec.elem);};
+        that.problem = function () { 
+            return $(elem).closest("section.problem");
+        };
+
+        checkbutton.onMouseOver(checkfn());
+        allElems.push(that);
+        return that;
+    };
+
+    // Find all jsinput elements, and create a jsinput object for each one
+    var walkDOM = function () {
+        var all = $(document).find('section[class="jsinput"]');
+        var newid;
+        for (i = 0; i++; i < all.length) {
+            // Get just the mako variable 'id' from the id attribute
+            newid = all[i].getAttribute("id").replace(/^inputtype_/, "");
+            var newJsElem = jsinputConstructor({
+                id: newid ,
+                elem: all[i]
+            });
+        }
+    };
+
+}).call(this);
