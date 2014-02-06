b029152800ca03b6c16b8b94dee018c4a7e2cfa8
yarko <yarkot1@gmail.com>
2014-02-06 02:55:58 -0600

    initial hack: need to handle custom theme processing, even if cms not recipient of them

--- cms/startup.py
+++ cms/startup.py
@@ -1,12 +1,14 @@
 """
-Module with code executed during Studio startup
+Module for code that should run during LMS startup
 """
+
 from django.conf import settings
 
 # Force settings to run so that the python path is modified
 settings.INSTALLED_APPS  # pylint: disable=W0104
 
 from django_startup import autostartup
+import edxmako
 
 
 def run():
@@ -14,3 +16,39 @@ def run():
     Executed during django startup
     """
     autostartup()
+
+    if settings.FEATURES.get('USE_CUSTOM_THEME', False):
+        enable_theme()
+
+
+def enable_theme():
+    """
+    Enable the settings for a custom theme, whose files should be stored
+    in ENV_ROOT/themes/THEME_NAME (e.g., edx_all/themes/stanford).
+    """
+    # Workaround for setting THEME_NAME to an empty
+    # string which is the default due to this ansible
+    # bug: https://github.com/ansible/ansible/issues/4812
+    theme_name = getattr(settings, "THEME_NAME", "")
+    if theme_name == "":
+        settings.THEME_NAME = None
+        return
+
+    assert settings.FEATURES['USE_CUSTOM_THEME']
+    settings.FAVICON_PATH = 'themes/{name}/images/favicon.ico'.format(
+        name=theme_name
+    )
+
+    # Calculate the location of the theme's files
+    theme_root = settings.ENV_ROOT / "themes" / settings.THEME_NAME
+
+    # Include the theme's templates in the template search paths
+    settings.TEMPLATE_DIRS.append(theme_root / 'templates')
+    settings.MAKO_TEMPLATES['main'].append(theme_root / 'templates')
+    edxmako.startup.run()
+
+    # Namespace the theme's static files to 'themes/<theme_name>' to
+    # avoid collisions with default edX static files
+    settings.STATICFILES_DIRS.append(
+        (u'themes/{}'.format(settings.THEME_NAME), theme_root / 'static')
+    )
